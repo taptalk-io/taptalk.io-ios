3a1cba9b55784266324ffcec36b57042396f9d0c
Dominic Vedericho <>
2018-12-19 12:37:23 +0700

    adding version 0.1.5 engine (add room list controller lifecycle delegate)

--- TapTalk/View Controllers/TAPRoomListViewController.h
+++ TapTalk/View Controllers/TAPRoomListViewController.h
@@ -8,10 +8,25 @@
 
 #import "TAPBaseViewController.h"
 
+@protocol TAPRoomListViewControllerLifecycleDelegate <NSObject>
+    
+@optional
+- (void)TAPRoomListViewControllerLoadView;
+- (void)TAPRoomListViewControllerViewDidLoad;
+- (void)TAPRoomListViewControllerViewWillAppear;
+- (void)TAPRoomListViewControllerViewWillDisappear;
+- (void)TAPRoomListViewControllerViewDidAppear;
+- (void)TAPRoomListViewControllerViewDidDisappear;
+- (void)TAPRoomListViewControllerDealloc;
+- (void)TAPRoomListViewControllerDidReceiveMemoryWarning;
+@end
+
 @interface TAPRoomListViewController : TAPBaseViewController
 
 @property (nonatomic) BOOL isViewAppear;
 @property (nonatomic) BOOL isShouldNotLoadFromAPI;
+@property (weak, nonatomic) id<TAPRoomListViewControllerLifecycleDelegate> lifecycleDelegate;
+
 
 - (void)viewLoadedSequence;
 
