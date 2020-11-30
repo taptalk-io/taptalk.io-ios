0b6cd965704c3bc1d7d73520d39c13ebf2f2ea96
alfian0 <alfian.official.mail@gmail.com>
2020-11-30 21:40:00 +0700

    Delete Pods

--- Pods/Target Support Files/Pods-TapTalk/Pods-TapTalk-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TapTalk : NSObject
-@end
-@implementation PodsDummy_Pods_TapTalk
-@end
--- Pods/Target Support Files/ZSWTappableLabel/ZSWTappableLabel-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ZSWTappableLabel : NSObject
-@end
-@implementation PodsDummy_ZSWTappableLabel
-@end
